e5af3c3566f1a8dfabd20431c1636a6326e1d8e4
Farsath <farsath2001@gmail.com>
2026-06-27 16:13:21 +0400

    fix WhatsappButton import path

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ import AdminLeads from './pages/admin/AdminLeads'
 import AdminRoute from './components/admin/AdminRoute'
 import { AdminProvider } from './context/AdminContext'
 import Agents from './pages/Agent'
-import WhatsAppButton from './components/common/WhatsAppButton'
+import WhatsAppButton from './components/common/WhatsappButton'
 import FAQPage from './pages/FAQPage'
 import NewsletterPage from './pages/NewsletterPage'
 
